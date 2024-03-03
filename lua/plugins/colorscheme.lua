ca7caca2e020fd64fd023b8980abf45a1e29b097
Erwin <e.kuechl@gmail.com>
2024-03-03 23:42:57 +0100

    mynvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+	return {
+	--{ "ellisonleao/gruvbox.nvim" },
+	--{'tanvirtin/monokai.nvim'},
+	--{'lunarvim/darkplus.nvim'},	
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {style = "storm"},
+	} 
+
